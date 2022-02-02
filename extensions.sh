f01f6e6c1243e433ff24275ca0e55ec62ab7f70a
Mukul Sharma <mymukul112@gmail.com>
2022-02-02 18:41:51 +0530

    Create extensions.sh

---
+++ extensions.sh
@@ -0,0 +1,28 @@
+#! bin/bash
+
+echo "
+***        *** ***      *** **    **  ***     ***  ***
+*** **  ** *** ***      *** **  **    ***     ***  ***
+***   **   *** ***      *** ****      ***     ***  ***
+***        *** ***      *** **  **    ***     ***  ***
+***        *** ************ **    **  ***********  ***********
+***        *** ************ **    **  ***********  ***********
+"
+
+echo "
+Follow me on GitHub => https://github.com/mukul98s
+Follow me on Linkedin => https://in.linkedin.com/in/mukul98s
+"
+
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-vscode.cpptools
+code --install-extension naumovs.color-highlight
+code --install-extension vscodevim.vim
+code --install-extension ritwickdey.liveserver
+code --install-extension eamodio.gitlens
+code --install-extension aaron-bond.better-comments
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension wix.vscode-import-cost
